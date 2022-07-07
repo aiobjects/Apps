881ec096a73dd58e3641287d05256099f6356a0e
Sriram <sriramch28@outlook.com>
2022-07-07 23:02:55 +0530

    Terraform files added

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,13 +1,10 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
-
 resource "aws_instance" "instance-1" {
-
-  ami           = "ami-006d3995d3a6b963b"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "RAJA-TEST"
-  }
+  ami           = var.AMIS[var.region]
+  instance_type = var.type
+  tags          = var.tags
 }
+
 
